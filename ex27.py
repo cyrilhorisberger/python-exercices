a43e9356999594bc68dffe4104cbca9a3f8ab224
Cyril Horisberger <cyril.horisberger@gmail.com>
2021-11-24 21:06:29 +0100

    Added song exercice

---
+++ ex27.py
@@ -0,0 +1,10 @@
+employee_name = ["Cyril Horisberger", "Marc-André Duruz", "Fanny Horisberger"]
+print (employee_name[0])
+
+song_name=["I believe I can fly","Again and again","Ouverture","Halleluja"]
+print (song_name[0])
+print (song_name[1])
+print (song_name[2])
+print (song_name[3])
+song_name [1] = "Take the shakels of my feet"
+print (song_name[1])
